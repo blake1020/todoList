5b39eb33cdbc42393f2901eec016f640b2d2cced
blake1020 <leandreiablakemore@rocketmail.com>
2024-06-13 11:32:08 -0500

    added event listener to button

--- index.js
+++ index.js
@@ -4,3 +4,11 @@
 const button = (document.getElementById('add-todo'));
 
 console.log(button);
+
+
+//Add to do Function when button is clicked
+function handleAddTodo() {
+console.log("button is clicked")
+}
+//Add an eventListener
+button.addEventListener('click', handleAddTodo);
