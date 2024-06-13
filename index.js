142a3ff3220de13d4441ca1115156cdc36cde5a0
blake1020 <leandreiablakemore@rocketmail.com>
2024-06-13 12:14:49 -0500

    added todo to todo list

--- index.js
+++ index.js
@@ -8,7 +8,28 @@ console.log(button);
 
 //Add to do Function when button is clicked
 function handleAddTodo() {
-console.log("button is clicked")
+//identifying the element i want to add items to 
+    const ul = document.querySelector('ul');
+console.log(ul);
+//create an li item to unorder list
+const li = document.createElement('li'); //creating new list item
+console.log("created list item", li);
+//access the input element
+const input = document.querySelector('input');
+console.log(input.value)
+//set the input text to my list items text
+li.textContent = input.value;
+
+//append my  list item to unorder list (as a child element)
+ul.appendChild(li)
+
 }
+
+
+
+
+
+
 //Add an eventListener
 button.addEventListener('click', handleAddTodo);
+
